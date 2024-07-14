fed2e13c298d4624be60117e02e84207b0ad6a75
t-nician <dev@senpai.zone>
2024-07-14 16:42:46 -0400

    epic code if i do say so myself.

--- src/cvg/socket/event.py
+++ src/cvg/socket/event.py
@@ -1,11 +1,16 @@
 from enum import Enum
 from dataclasses import dataclass, field
-    
+
+
+CHANNEL_ALREADY_EXISTS = "Channel '{0}' already exists!"
+CHANNEL_DOESNT_EXIST = "Channel '{0}' doesn't exist!"
+
 
 @dataclass
 class Event:
-    channel: any = field(default=None)
-    returns: bool = field(default=False)
+    channel_name: any = field(default=None)
+    
+    func: ( ) = field(default=lambda _: ())
 
 
 @dataclass
@@ -19,10 +24,13 @@ class Channel:
 
 @dataclass
 class Pool:
-    channels: dict[any, Channel] = field(default_factory=list)
+    channels: dict[any, Channel] = field(default_factory=dict)
     
     def add_channel(self, channel: Channel):
-        assert self.channels.get(channel.name) is None, ""
+        assert self.channels.get(channel.name) is None, CHANNEL_ALREADY_EXISTS.format(
+            str(channel.name)
+        )
+        
         self.channels[channel.name] = channel
         
         def wrapper(func):
@@ -32,6 +40,34 @@ class Pool:
     
     def add_event(self, event: Event):
         def wrapper(func):
-            print("event time")
+            channel = self.channels.get(event.channel_name)
+            
+            assert channel, CHANNEL_DOESNT_EXIST.format(
+                str(event.channel_name)
+            )
+            
+            event.func = func
+            channel.listeners.append(event)
         
         return wrapper
+    
+    def emit(self, channel_name: any, *args, **kwargs):
+        channel = self.channels.get(channel_name)
+        args, kwargs = channel.transformer_func(*args, **kwargs)
+        
+        assert channel, CHANNEL_DOESNT_EXIST.format(
+            str(channel_name)
+        )
+        
+        compiled_return = []
+        singular_return = None
+        
+        for listener in channel.listeners:
+            result = listener.func(*args, **kwargs)
+            
+            if channel.compile_returns and result is not None:
+                compiled_return.extend(list(result))
+            elif singular_return is None and result is not None:
+                singular_return = result
+        
+        return channel.compile_returns and compiled_return or singular_return
--- src/main.py
+++ src/main.py
@@ -4,15 +4,20 @@ TEST_CHANNEL = b"TEST"
 
 pool = event.Pool()
 
-
 @pool.add_channel(event.Channel(TEST_CHANNEL))
 def channel_transformer(*args, **kwargs):
     print("transform time!")
     return args, kwargs
 
-
 @pool.add_event(event.Event(TEST_CHANNEL))
 def event_listener(*args, **kwargs):
     print("event time!")
-    return args, kwargs
+    return "hello", "there"
 
+@pool.add_event(event.Event(TEST_CHANNEL))
+def other_listener(*args, **kwargs):
+    print("other time!")
+    return "test", "this"
+
+
+print(pool.emit(TEST_CHANNEL))
